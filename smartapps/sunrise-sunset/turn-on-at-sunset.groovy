e6fa1828f78160e614b1eed2f07d2281e7949a0e
Chuck <ccwrobel@hotmail.com>
2019-12-29 21:16:28 -0800

    Update turn-on-at-sunset.groovy
    added flash on/off

--- smartapps/sunrise-sunset/turn-on-at-sunset.groovy
+++ smartapps/sunrise-sunset/turn-on-at-sunset.groovy
@@ -14,18 +14,23 @@
  *
  */
 definition(
-    name: "Turn on at Sunset",
-    namespace: "examples",
-    author: "SmartThings",
-    description: "Turn on lights at sunset, based on your location's geofence.",
+    name: "Turn on at Sunset and check if garage door is open",
+    namespace: "chuckster53",
+    author: "chuck",
+    description: "flash ights at sunset, if garage door is open",
     category: "My Apps",
     iconUrl: "https://s3.amazonaws.com/smartapp-icons/ModeMagic/rise-and-shine.png",
     iconX2Url: "https://s3.amazonaws.com/smartapp-icons/ModeMagic/rise-and-shine@2x.png",
     iconX3Url: "https://s3.amazonaws.com/smartapp-icons/ModeMagic/rise-and-shine@2x.png")
 
 preferences {
+    section("When any of the following devices trigger..."){
+		input "contact", "capability.contactSensor", title: "Contact Sensor?", required: false
+    }
+   
     section("Lights") {
-        input "switches", "capability.switch", title: "Which lights to turn on?"
+        input "switches", "capability.switch", title: "Which lights to flash?"
+        input "numFlashes", "number", title: "This number of times (default 3)", required: false
     }
 }
 
@@ -40,9 +45,57 @@ def updated() {
 
 def initialize() {
     subscribe(location, "sunset", sunsetHandler)
+    if (contact) {
+		subscribe(contact, "contact.open", contactOpenHandler)
+	}
 }
 
 def sunsetHandler(evt) {
     log.debug "turning on lights at sunset"
-    switches.on()
+    flashLights()
+}
+
+private flashLights() {
+	def doFlash = true
+	def onFor = onFor ?: 1000
+	def offFor = offFor ?: 1000
+	def numFlashes = numFlashes ?: 3
+
+	log.debug "LAST ACTIVATED IS: ${state.lastActivated}"
+	if (state.lastActivated) {
+		def elapsed = now() - state.lastActivated
+		def sequenceTime = (numFlashes + 1) * (onFor + offFor)
+		doFlash = elapsed > sequenceTime
+		log.debug "DO FLASH: $doFlash, ELAPSED: $elapsed, LAST ACTIVATED: ${state.lastActivated}"
+	}
+
+	if (doFlash) {
+		log.debug "FLASHING $numFlashes times"
+		state.lastActivated = now()
+		log.debug "LAST ACTIVATED SET TO: ${state.lastActivated}"
+		def initialActionOn = switches.collect{it.currentSwitch != "on"}
+		def delay = 0L
+		numFlashes.times {
+			log.trace "Switch on after  $delay msec"
+			switches.eachWithIndex {s, i ->
+				if (initialActionOn[i]) {
+					s.on(delay: delay)
+				}
+				else {
+					s.off(delay:delay)
+				}
+			}
+			delay += onFor
+			log.trace "Switch off after $delay msec"
+			switches.eachWithIndex {s, i ->
+				if (initialActionOn[i]) {
+					s.off(delay: delay)
+				}
+				else {
+					s.on(delay:delay)
+				}
+			}
+			delay += offFor
+		}
+	}
 }
